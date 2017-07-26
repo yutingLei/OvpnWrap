8d6f8af826dc53386229048b699ca744b095db5e
yutingLei <lyt_930617@163.com>
2017-07-26 11:04:42 +0800

    Initial Commit

---
+++ OvpnWrap/OvpnWrap.h
@@ -0,0 +1,19 @@
+//
+//  OvpnWrap.h
+//  OvpnWrap
+//
+//  Created by admin on 7/26/17.
+//  Copyright © 2017 Yt-L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OvpnWrap.
+FOUNDATION_EXPORT double OvpnWrapVersionNumber;
+
+//! Project version string for OvpnWrap.
+FOUNDATION_EXPORT const unsigned char OvpnWrapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OvpnWrap/PublicHeader.h>
+
+
